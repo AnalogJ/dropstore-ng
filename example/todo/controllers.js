6f70f90005ccfc52f0b0f53811e6c9723219e7b4
Jason Kulatunga <jk17@ualberta.ca>
2013-07-10 10:27:23 -0700

    removed unneeded safeApply reference.

--- example/todo/controllers.js
+++ example/todo/controllers.js
@@ -7,7 +7,7 @@
  */
 angular.module('testApp.controllers', [])
     //Panel Controllers
-    .controller('pageTestCtrl', function ($scope, $rootScope,safeApply, dropstoreClient) {
+    .controller('pageTestCtrl', function ($scope, $rootScope, dropstoreClient) {
         var _datastore = null;
         $scope.tasks = [];
         dropstoreClient.authenticate({interactive: true})
@@ -30,18 +30,6 @@ angular.module('testApp.controllers', [])
                     }
                 },'tasks');
 
-                /*var fqtopic = datastore.subscribe('tasks');
-
-                $rootScope.$on(fqtopic, function(event, records) {
-                    console.log('record added', records);
-                    for(var ndx in records){
-                        console.log(records[ndx].get('taskname'), 'current tasks', $scope.tasks);
-
-                        $scope.tasks.push(records[ndx]);
-                    }
-                    safeApply($scope); //todo:for some reason the $on function is not updating the scope properly when updates occur on non-local browser.
-                });
-                */
                 var taskTable = datastore.getTable('tasks');
                 $scope.tasks =  taskTable.query();
             })
